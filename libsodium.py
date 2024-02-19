795cee904cd769a535453f4ac2aee1fe3204753f
ddomnicraj <54891544+ddomnicraj@users.noreply.github.com>
2024-02-19 21:24:04 +0530

    Update libsodium.py

--- libsodium.py
+++ libsodium.py
@@ -9,4 +9,4 @@ def encrypt(public_key: str, secret_value: str) -> str:
   return b64encode(encrypted).decode("utf-8")
 
 encrypted_secret = encrypt("Y/FZ56RUjOHbfO+8/Kh/EORaYrdEtkVtKuO3qZV0LRk=", "team_new")
-print("Encrypted secret=",encrypted_secret)
+print(encrypted_secret)
