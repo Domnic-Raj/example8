313de3d74db256ffd5c405c34d43835c583f183e
ddomnicraj <54891544+ddomnicraj@users.noreply.github.com>
2024-02-20 19:25:20 +0530

    Update key.sh

--- key.sh
+++ key.sh
@@ -2,7 +2,7 @@
 export ORG_NAME="Domnic-Raj"
 #export GITHUB_TOKEN="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"
 export GITHUB_TOKEN="$GH_TKN"
-secret=$sec 
+export secret=$sec 
 echo "secret=$sec">> env_vars.sh
 while read line; do
 API_URL="https://api.github.com/repos/$ORG_NAME/$line/actions/secrets/public-key"
@@ -10,7 +10,7 @@ API_URL1="https://api.github.com/repos/$ORG_NAME/$line/actions/secrets/put"
 RESPONSE=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" "$API_URL")
 echo " $RESPONSE"
 key_id=$(echo "$RESPONSE" | jq -r '.key_id')
-key=$(echo "$RESPONSE" | jq -r '.key')
+export key=$(echo "$RESPONSE" | jq -r '.key')
 #echo "key_id=$(echo "$RESPONSE" | jq -r '.key_id')"
 echo "key=$(echo "$RESPONSE" | jq -r '.key')" >> env_vars.sh
 . ./env_vars.sh
