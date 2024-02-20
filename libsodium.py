8f94feb40a7f8b16fd8616d3536816b930d6cd8b
ddomnicraj <54891544+ddomnicraj@users.noreply.github.com>
2024-02-20 18:20:23 +0530

    Update libsodium.py

--- libsodium.py
+++ libsodium.py
@@ -1,6 +1,10 @@
+import os
+
 from base64 import b64encode
 from nacl import encoding, public
 
+key = os.getenv("key")
+#print(key)
 def encrypt(public_key: str, secret_value: str) -> str:
   """Encrypt a Unicode string using the public key."""
   public_key = public.PublicKey(public_key.encode("utf-8"), encoding.Base64Encoder())
@@ -8,5 +12,7 @@ def encrypt(public_key: str, secret_value: str) -> str:
   encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
   return b64encode(encrypted).decode("utf-8")
 
-encrypted_secret = encrypt("Y/FZ56RUjOHbfO+8/Kh/EORaYrdEtkVtKuO3qZV0LRk=", "team_new")
+encrypted_secret = encrypt(key, "team_new1")
 print(encrypted_secret)
+with open("env_vars.sh", "w") as file:
+    file.write(f"export encrypted_secret={encrypted_secret}\n")
