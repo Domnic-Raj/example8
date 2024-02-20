4a0c12feaee4706791f10f8278474584b048020a
ddomnicraj <54891544+ddomnicraj@users.noreply.github.com>
2024-02-20 19:08:53 +0530

    Update key.sh

--- key.sh
+++ key.sh
@@ -2,19 +2,19 @@
 export ORG_NAME="Domnic-Raj"
 #export GITHUB_TOKEN="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"
 export GITHUB_TOKEN="$GH_TKN"
-secret=$sec
+secret=$sec > env_vars.sh
 while read line; do
 API_URL="https://api.github.com/repos/$ORG_NAME/$line/actions/secrets/public-key"
 API_URL1="https://api.github.com/repos/$ORG_NAME/$line/actions/secrets/put"
 RESPONSE=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" "$API_URL")
 echo " $RESPONSE"
 key_id=$(echo "$RESPONSE" | jq -r '.key_id')
-key=$(echo "$RESPONSE" | jq -r '.key') > env_vars.sh
+key=$(echo "$RESPONSE" | jq -r '.key') >> env_vars.sh
 #echo "key_id=$(echo "$RESPONSE" | jq -r '.key_id')"
 #echo "key=$(echo "$RESPONSE" | jq -r '.key')"
 source ./env_vars.sh
 python3 libsodium.py
-source ./env_vars.sh
+source ./env_vars1.sh
 #INPUT="-d '{"encrypted_value":"$encrypted_secret","key_id":"$key_id"}'"
 INPUT='-d '{\"encrypted_value\":\"$encrypted_secret\",\"key_id\":\"$key_id\"}''
 echo " $INPUT"
