3faebe1ba23297c645a20d29af7a01f4424ccc65
ddomnicraj <54891544+ddomnicraj@users.noreply.github.com>
2024-02-20 19:16:14 +0530

    Update key.sh

--- key.sh
+++ key.sh
@@ -2,16 +2,17 @@
 export ORG_NAME="Domnic-Raj"
 #export GITHUB_TOKEN="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"
 export GITHUB_TOKEN="$GH_TKN"
-secret=$sec > env_vars.sh
+secret=$sec 
+echo "secret=$sec">> env_vars.sh
 while read line; do
 API_URL="https://api.github.com/repos/$ORG_NAME/$line/actions/secrets/public-key"
 API_URL1="https://api.github.com/repos/$ORG_NAME/$line/actions/secrets/put"
 RESPONSE=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" "$API_URL")
 echo " $RESPONSE"
 key_id=$(echo "$RESPONSE" | jq -r '.key_id')
-key=$(echo "$RESPONSE" | jq -r '.key') >> env_vars.sh
+key=$(echo "$RESPONSE" | jq -r '.key')
 #echo "key_id=$(echo "$RESPONSE" | jq -r '.key_id')"
-#echo "key=$(echo "$RESPONSE" | jq -r '.key')"
+echo "key=$(echo "$RESPONSE" | jq -r '.key')" >> env_vars.sh
 source ./env_vars.sh
 python3 libsodium.py
 source ./env_vars1.sh
