583bbceab0e623dfeb9ed6ef99f2cadfa5cc3f6e
ddomnicraj <54891544+ddomnicraj@users.noreply.github.com>
2024-02-20 18:59:28 +0530

    Update key.sh

--- key.sh
+++ key.sh
@@ -9,9 +9,10 @@ API_URL1="https://api.github.com/repos/$ORG_NAME/$line/actions/secrets/put"
 RESPONSE=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" "$API_URL")
 echo " $RESPONSE"
 key_id=$(echo "$RESPONSE" | jq -r '.key_id')
-key=$(echo "$RESPONSE" | jq -r '.key')
+key=$(echo "$RESPONSE" | jq -r '.key') > env_vars.sh
 #echo "key_id=$(echo "$RESPONSE" | jq -r '.key_id')"
 #echo "key=$(echo "$RESPONSE" | jq -r '.key')"
+source ./env_vars.sh
 python3 libsodium.py
 source ./env_vars.sh
 #INPUT="-d '{"encrypted_value":"$encrypted_secret","key_id":"$key_id"}'"
