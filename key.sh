26e1950e739406104e1b2423b33cc0a7b2c705f3
ddomnicraj <54891544+ddomnicraj@users.noreply.github.com>
2024-02-21 13:27:28 +0530

    Update key.sh

--- key.sh
+++ key.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+check_status() {
+    if [ $? -eq 0 ]; then
+        echo "Success: $1"
+    else
+        echo "Failure: $1"
+        exit 1
+    fi
+}
+
 export ORG_NAME="Domnic-Raj"
 #export GITHUB_TOKEN="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"
 export GITHUB_TOKEN="$GH_TKN"
@@ -8,6 +18,7 @@ while read line; do
 API_URL="https://api.github.com/repos/$ORG_NAME/$line/actions/secrets/public-key"
 API_URL1="https://api.github.com/repos/$ORG_NAME/$line/actions/secrets/put"
 RESPONSE=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" "$API_URL")
+check_status "Getting public key for $line"
 echo " $RESPONSE"
 key_id=$(echo "$RESPONSE" | jq -r '.key_id')
 export key=$(echo "$RESPONSE" | jq -r '.key')
@@ -15,10 +26,12 @@ export key=$(echo "$RESPONSE" | jq -r '.key')
 #echo "key=$(echo "$RESPONSE" | jq -r '.key')" >> env_vars.sh
 #. ./env_vars.sh
 python3 libsodium.py
+check_status "Running Python script for $line"
 #. ./env_vars1.sh
 . ./env_vars.sh
 #INPUT="-d '{"encrypted_value":"$encrypted_secret","key_id":"$key_id"}'"
 INPUT='-d '{\"encrypted_value\":\"$encrypted_secret\",\"key_id\":\"$key_id\"}''
 echo " $INPUT"
 RESPONSE1=$(curl -L -X PUT -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" "$API_URL1" "${INPUT}")
+check_status "Updating secret for $line"
 done < inv.txt
