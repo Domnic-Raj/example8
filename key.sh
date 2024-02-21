3cf59f47d87fc5b9e427ce14d4c306c91823a46e
ddomnicraj <54891544+ddomnicraj@users.noreply.github.com>
2024-02-21 13:33:21 +0530

    Update key.sh

--- key.sh
+++ key.sh
@@ -17,9 +17,9 @@ export secret=$sec
 while read line; do
 API_URL="https://api.github.com/repos/$ORG_NAME/$line/actions/secrets/public-key"
 API_URL1="https://api.github.com/repos/$ORG_NAME/$line/actions/secrets/put"
-RESPONSE=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" "$API_URL")
+RESPONSE=$(curl -sSL -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" "$API_URL")
 check_status "Getting public key for $line"
-echo " $RESPONSE"
+#echo " $RESPONSE"
 key_id=$(echo "$RESPONSE" | jq -r '.key_id')
 export key=$(echo "$RESPONSE" | jq -r '.key')
 #echo "key_id=$(echo "$RESPONSE" | jq -r '.key_id')"
@@ -31,7 +31,7 @@ check_status "Running Python script for $line"
 . ./env_vars.sh
 #INPUT="-d '{"encrypted_value":"$encrypted_secret","key_id":"$key_id"}'"
 INPUT='-d '{\"encrypted_value\":\"$encrypted_secret\",\"key_id\":\"$key_id\"}''
-echo " $INPUT"
-RESPONSE1=$(curl -L -X PUT -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" "$API_URL1" "${INPUT}")
+#echo " $INPUT"
+RESPONSE1=$(curl -sSL -X PUT -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" "$API_URL1" "${INPUT}")
 check_status "Updating secret for $line"
 done < inv.txt
